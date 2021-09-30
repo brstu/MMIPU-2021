605ba717f7e37bcb05e0ac61664bca11579728fc
Daria Zinkova <dasha.zink@mail.ru>
2021-09-30 15:06:41 +0300

    clean

--- trunk/as005505/task_01/src/main.cpp
+++ trunk/as005505/task_01/src/main.cpp
@@ -1,17 +1,1 @@
-#include "stdafx.h"
-#include "linear.h"
-#include "notlinear.h"
-#include <iostream>
-
-using namespace std;
-
-
-int main() {
-	setlocale(0, "");
-	linear lin;
-	notlinear notlin;
-	lin.ShowResults();
-	cout << endl << endl;
-	notlin.ShowResults();
-	return 0;
-}
+// Здесь должен быть код, но его нет
