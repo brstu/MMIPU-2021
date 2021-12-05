7c319d1760d81c62f0a667b2eda7ec33e49533b3
Andrey Kuchko <90598162+Kuchko-Andrey@users.noreply.github.com>
2021-12-05 15:53:42 +0300

    Add files via upload

---
+++ trunk/as005508/task_03/src/lab3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab3.cpp
+// Author      : Andrey Kuchko
+// Version     :
+// Copyright   : 
+// Description : completed the task on laboratory work 3 Andrey Kuchko
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
