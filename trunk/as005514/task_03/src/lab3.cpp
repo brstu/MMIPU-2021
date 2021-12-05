452ae6d7695aef8e6b1696b167cbb7a610572bdb
Eugene Nikitenkov <90552354+Eugene-Nikitenkov@users.noreply.github.com>
2021-12-05 15:54:52 +0300

    Add files via upload

---
+++ trunk/as005514/task_03/src/lab3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab3.cpp
+// Author      : Eugene Nik
+// Version     :
+// Copyright   : 
+// Description : completed the task on laboratory work 3 Eugene Nik
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
