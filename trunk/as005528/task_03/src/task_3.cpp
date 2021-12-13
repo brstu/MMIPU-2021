bb4da7cb6e4c55c44eb4da5641ee673215233ba0
Khmaryk Darya <85825739+daryakhmaryk@users.noreply.github.com>
2021-12-13 11:56:00 +0300

    Лабораторная работа №3 (#129)

---
+++ trunk/as005528/task_03/src/task_3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : task_3.cpp
+// Author      : Darya Khmaryk
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
