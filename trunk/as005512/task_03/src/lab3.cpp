f0fc82cd6aae17a8457d035deb4f59d659254d65
Sergey Martynovich <pelmeni.pro@gmail.com>
2021-12-13 12:07:53 +0300

    files fixed

---
+++ trunk/as005512/task_03/src/lab3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab3.cpp
+// Author      : Martynovich Sergey
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // вывод "Hello World!"
+	return 0;
+}
