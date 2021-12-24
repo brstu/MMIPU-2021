2a414d32495e11472c08310a78232d8e3592d86a
Dan9 <90175539+Mamkin-kovboi@users.noreply.github.com>
2021-12-24 19:13:39 +0300

    Create lab3.cpp

---
+++ trunk/AS005531/task_03/src/lab3.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab3.cpp
+// Author      : Daniil Darkovich
+// Version     :
+// Copyright   : 
+// Description : completed the task on laboratory work 3 Daniil Darkovich
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
